c2e555ba93be41d9079e05a37b2e30444930e550
fasasisherif <59504098+fasasisherif@users.noreply.github.com>
2020-03-30 10:04:00 +0100

    Add files via upload

---
+++ PalindromeChecker.js
@@ -0,0 +1,43 @@
+you = "Sherif"
+
+function myfunc(word){
+    var check = [you];
+    var a = check.length-1;
+    var b = you.length;
+    
+    console.log("We are checking "+ you + " which is a "+ b + " letter word if it is palindrome or not" )
+    var b = you.length-1;
+
+    c = [check[a][b]];
+    b--;
+    d = [check[a][b]];
+    b--;
+    e = [check[a][b]];
+    b--;
+    f = [check[a][b]];
+    b--;
+    g = [check[a][b]];
+    b--;
+    h = [check[a][b]];
+
+    var x = c.concat(d,e,f,g,h);
+    var y = x.join('')
+    console.log(y);
+
+    if (y == you ){
+        console.log("This is a palindrome word")
+    }
+    if (y != you ){
+        console.log("This not a palindrome word")
+    }
+}
+
+myfunc(you)
+
+
+
+
+
+
+
+
